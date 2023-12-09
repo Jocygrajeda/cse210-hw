11b19a43ddfca25c44f051cf4575466c28669d44
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2023-12-08 23:36:05 -0600

    update

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 
 class Program
@@ -20,6 +20,53 @@ class Program
         // Create orders
         Order order1 = new Order(new List<Product> { product1, product2 }, usaCustomer);
         Order order2 = new Order(new List<Product> { product2 }, nonUsaCustomer);
+
+        // Display results
+        DisplayOrderInformation(order1, "Order 1");
+        DisplayOrderInformation(order2, "Order 2");
+    }
+
+    static void DisplayOrderInformation(Order order, string orderName)
+    {
+        Console.WriteLine($"{orderName} - Packing Label:");
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine();
+
+        Console.WriteLine($"{orderName} - Shipping Label:");
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine();
+
+        Console.WriteLine($"{orderName} - Total Price: ${order.CalculateTotalPrice()}");
+        Console.WriteLine("\n----------------------\n");
+    }
+}
+*/
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create addresses
+        Address usaAddress = new Address("123 Main St", "Anytown", "CA", "USA");
+        Address nonUsaAddress = new Address("456 International St", "Cityville", "Provinceville", "Canada");
+
+        // Create customers
+        Customer usaCustomer = new Customer("John Doe", usaAddress);
+        Customer nonUsaCustomer = new Customer("Jane Smith", nonUsaAddress);
+
+        // Create products
+        Product product1 = new Product("Laptop", "ABC123", 1200, 2);
+        Product product2 = new Product("Printer", "DEF456", 300, 1);
+
+        // Create orders
+        Order order1 = new Order(new List<Product> { product1, product2 }, usaCustomer);
+
+        // Add another product to the Canadian order
+        Product product3 = new Product("Keyboard", "LJC1119", 300, 2);
+        Order order2 = new Order(new List<Product> { product2, product3 }, nonUsaCustomer);
 
         // Display results
         DisplayOrderInformation(order1, "Order 1");
